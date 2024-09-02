97eaea5d02775f2116bb7be8ca1320faa9eb2a72
Arkaprava Ghosh <arkapravaghosh99@gmail.com>
2024-09-03 00:23:22 +0530

    Update UI Part for attachments

--- Xpenso/Views/ExpenseDetailView.swift
+++ Xpenso/Views/ExpenseDetailView.swift
@@ -11,17 +11,17 @@ struct ExpenseDetailOptionView : View {
             Button(action: action, label: {
                 HStack {
                     image
-                        .foregroundStyle(.primary)
+                        .foregroundStyle(AppTheme.shared.selectedColor)
                     Text(title)
                         .setCustomFont(weight: .medium)
-                        .foregroundStyle(.primary)
+                        .foregroundStyle(AppTheme.shared.selectedColor)
                 }
             })
             .padding(12)
             .overlay {
                 RoundedRectangle(cornerRadius: 10)
-                    .stroke(Color.primary, lineWidth: 1)
-                    .foregroundStyle(Color.clear)
+                    .stroke(Color.gray, lineWidth: 1)
+                    .foregroundStyle(AppTheme.shared.selectedColor)
                 
             }
             
@@ -34,8 +34,11 @@ struct ExpenseDetailView: View {
     @State var showAddComment = false
     
     @State var comments = [Comment]()
+//    @State var attachments = [Attachment]()
     
     var expense: Expense
+    @State private var selectedSegment: Int = 0
+    let segments = ["Attachments", "Comments"]
     var commentViewModel : CommentService = CommentServiceImpl()
     
     init(expense: Expense) {
@@ -47,55 +50,28 @@ struct ExpenseDetailView: View {
     var body: some View {
         
         VStack {
-            ScrollView {
-                VStack {
-                    HStack {
-                        
-                        VStack(alignment: .leading) {
-                            Text(expense.amount, format: .currency(code: "INR"))
-                                .setCustomFont(size: UIFont.preferredFont(forTextStyle: .title3).pointSize)
+            List {
+                Section {
+                    VStack {
+                        HStack {
                             
-                            if let desc = expense.desc {
-                                Text("\(desc)")
-                                    .lineLimit(0)
-                                    .setCustomFont(size: UIFont.preferredFont(forTextStyle: .body).pointSize)
-                                    .foregroundStyle(.secondary)
-                            }
-                            
-                            
-                            HStack {
-                                Image(expense.category.rawValue, bundle: nil)
-                                    .renderingMode(.template)
-                                    .resizable()
-                                    .frame(width: 18, height: 18)
-                                    .foregroundStyle(.secondary)
+                            VStack(alignment: .leading) {
+                                Text(expense.amount, format: .currency(code: "INR"))
+                                    .setCustomFont(size: UIFont.preferredFont(forTextStyle: .title3).pointSize)
                                 
-                                Image("dot", bundle:nil)
-                                    .renderingMode(.template)
-                                    .resizable()
-                                    .frame(width: 10, height: 10)
-                                    .rotationEffect(.degrees(90))
-                                    .foregroundStyle(Color.secondary)
-                                
-                                Image("calender", bundle: nil)
-                                    .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
-                                    .resizable()
-                                    .frame(width: 18, height: 18)
-                                    .foregroundStyle(.secondary)
-                                
-                                if let date = expense.date {
-                                    Text("\(getFormattedDate(date: date))")
+                                if let desc = expense.desc {
+                                    Text("\(desc)")
+                                        .lineLimit(0)
+                                        .setCustomFont(size: UIFont.preferredFont(forTextStyle: .body).pointSize)
+                                        .foregroundStyle(.secondary)
                                 }
-                            }
-                            
-                            if !comments.isEmpty {
+                                
                                 HStack {
-                                    Image("comments", bundle:nil)
+                                    Image(expense.category.rawValue, bundle: nil)
                                         .renderingMode(.template)
                                         .resizable()
                                         .frame(width: 18, height: 18)
-                                        .rotationEffect(.degrees(90))
-                                        .foregroundStyle(Color.secondary)
+                                        .foregroundStyle(.secondary)
                                     
                                     Image("dot", bundle:nil)
                                         .renderingMode(.template)
@@ -103,48 +79,133 @@ struct ExpenseDetailView: View {
                                         .frame(width: 10, height: 10)
                                         .rotationEffect(.degrees(90))
                                         .foregroundStyle(Color.secondary)
-                                    Text("\(comments.count)")
                                     
-                                        .setCustomFont()
+                                    Image("calender", bundle: nil)
+                                        .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                                        .resizable()
+                                        .frame(width: 18, height: 18)
+                                        .foregroundStyle(.secondary)
                                     
+                                    if let date = expense.date {
+                                        Text("\(getFormattedDate(date: date))")
+                                    }
                                 }
-                                .padding(.bottom, 16)
                                 
-                                ForEach(comments) {
-                                    comment in
-                                    HStack {
-                                        Image("comments", bundle: nil)
-                                            .resizable()
-                                            .frame(width: 18, height: 18)
-                                            .foregroundStyle(.secondary)
-                                            .padding(.trailing, 8)
-                                        VStack(alignment: .leading) {
-                                            Text("\(comment.comment)")
-                                                .setCustomFont()
-                                                .padding(.bottom, 4)
-                                            Text("\(getFormattedDate(date: comment.createdTime))")
-                                                .setCustomFont()
-                                                .foregroundStyle(.secondary)
+                            }
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal, 8)
+                        
+                        Picker("Select an option", selection: $selectedSegment) {
+                                        ForEach(0..<segments.count) { index in
+                                            Text(segments[index])
+                                                .tag(index)
                                         }
                                     }
-//                                    .swipeActions {
-//                                        Button(role: .destructive) {
-//                                            deleteComment(comment: comment)
-//                                        } label: {
-//                                            Image("delete", bundle: nil)
-//                                                .renderingMode(.template)
-//                                        }
-//                                        .tint(Color.red)
-//                                    }
+                                    .pickerStyle(SegmentedPickerStyle())
+                 
+                    }
+                }
+                
+                if selectedSegment == 1 {
+                    if !comments.isEmpty {
+                        
+                        ForEach(comments) {
+                            comment in
+                            HStack {
+                                Image("comments", bundle: nil)
+                                    .resizable()
+                                    .frame(width: 18, height: 18)
+                                    .foregroundStyle(.secondary)
+                                    .padding(.trailing, 8)
+                                VStack(alignment: .leading) {
+                                    Text("\(comment.comment)")
+                                        .setCustomFont()
+                                        .padding(.bottom, 4)
+                                    Text("\(getFormattedDate(date: comment.createdTime))")
+                                        .setCustomFont()
+                                        .foregroundStyle(.secondary)
                                 }
                             }
+                            .swipeActions {
+                                Button(role: .destructive) {
+                                    deleteComment(comment: comment)
+                                } label: {
+                                    Image("delete", bundle: nil)
+                                        .renderingMode(.template)
+                                }
+                                .tint(Color.red)
+                            }
                         }
-                        Spacer()
                     }
-                    .padding(.horizontal, 8)
+                    else {
+                        HStack {
+                            
+                            Spacer()
+                            Image("comments", bundle: nil)
+                                .resizable()
+                                .frame(width: 18, height: 18)
+                            Text("No Comments")
+                                .setCustomFont()
+                            Spacer()
+                            
+                        }
+                        .padding(.vertical, 8)
+                        
+                    }
                 }
+//                else {
+//                   
+//                    if !attachments.isEmpty {
+//                        
+//                        ForEach(comments) {
+//                            comment in
+//                            HStack {
+//                                Image("comments", bundle: nil)
+//                                    .resizable()
+//                                    .frame(width: 18, height: 18)
+//                                    .foregroundStyle(.secondary)
+//                                    .padding(.trailing, 8)
+//                                VStack(alignment: .leading) {
+//                                    Text("\(comment.comment)")
+//                                        .setCustomFont()
+//                                        .padding(.bottom, 4)
+//                                    Text("\(getFormattedDate(date: comment.createdTime))")
+//                                        .setCustomFont()
+//                                        .foregroundStyle(.secondary)
+//                                }
+//                            }
+//                            .swipeActions {
+//                                Button(role: .destructive) {
+//                                    deleteComment(comment: comment)
+//                                } label: {
+//                                    Image("delete", bundle: nil)
+//                                        .renderingMode(.template)
+//                                }
+//                                .tint(Color.red)
+//                            }
+//                        }
+//                    }
+//                    else {
+//                        HStack {
+//                            
+//                            Spacer()
+//                            Image("attachmens", bundle: nil)
+//                                .resizable()
+//                                .frame(width: 18, height: 18)
+//                            Text("No Attachments")
+//                                .setCustomFont()
+//                            Spacer()
+//                            
+//                        }
+//                        .padding(.vertical, 8)
+//                        
+//                    }
+//                }
+    
+                
             }
-            Spacer()
             
             HStack {
                 ExpenseDetailOptionView(title: "Add Attachment", image: Image("attachment", bundle: nil)) {
@@ -185,6 +246,6 @@ struct ExpenseDetailView: View {
 
 #Preview {
     let config = ModelConfiguration(isStoredInMemoryOnly: true) // Store the container in memory since we don't actually want to save the preview data
-    let container = try! ModelContainer(for: Expense.self, configurations: config)
+    let container = try! ModelContainer(for: Expense.self, Comment.self, configurations: config)
     return ExpenseDetailView(expense: Expense.singleExpense)
 }
